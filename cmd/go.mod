df8dbc429aec26f6445afff3b8106337575c8ced
698un <ya698un@yandex.ru>
2022-09-10 16:44:11 +0300

    firstCommit

---
+++ cmd/go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.18
+
+//replace somemodule => ../somemodule
+
+//require somemodule v0.0.0-00010101000000-000000000000 // indirect
+
+
---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//fmt.Println(calc.AddInts(1, 2))
+	fmt.Println("test")
+
+	fmt.Println("The end")
+}
